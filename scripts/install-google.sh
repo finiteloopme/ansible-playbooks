7fa2b90e2a0d8c06032054f49e8a2761f7b22185
Kunal Limaye <kunallimaye@gmail.com>
2016-07-20 14:49:56 +1000

    Added the capability to remove VMs using ansible playbook

--- scripts/install-google.sh
+++ scripts/install-google.sh
@@ -1,2 +1,2 @@
 # !/bin/bash
-docker run -v `pwd`:/var/ansible finiteloop/ose-install-google ansible-playbook -i inventory/ose3 ose.yml
+docker run -v `pwd`:/var/ansible finiteloop/ose-install-google ansible-playbook -i inventory/ose3 ose-install.yml
